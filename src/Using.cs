5c91da5950d277f087843ee25bb585c0092e61a3
Huy Ngo <huybngo@yahoo.com>
2024-04-05 16:46:55 -0700

    Use callback from Blazor.Core

--- src/Using.cs
+++ src/Using.cs
@@ -1,6 +1,10 @@
 global using Microsoft.AspNetCore.Components;
 global using Microsoft.JSInterop;
 
-global using Blazor.FamilyTreeJS.Interop.CallbackInterops;
 global using Blazor.FamilyTreeJS.Interop.Events;
 global using Blazor.FamilyTreeJS.Interop.Options;
+
+global using Blazor.Core;
+global using Blazor.Core.Components;
+global using Blazor.Core.Interop;
+global using Blazor.Core.Interop.Callbacks;
