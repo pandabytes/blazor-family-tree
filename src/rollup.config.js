26d5268e8e4e17831bf9fd98cd89b22375835503
Huy Ngo <huybngo@yahoo.com>
2024-04-06 09:31:52 -0700

    Remove tab

--- src/rollup.config.js
+++ src/rollup.config.js
@@ -1,18 +1,18 @@
 import multiInput from 'rollup-plugin-multi-input';
 import terser from '@rollup/plugin-terser';
-import { nodeResolve } from '@rollup/plugin-node-resolve';
+import nodeResolve from '@rollup/plugin-node-resolve';
 import commonjs from "@rollup/plugin-commonjs";
 
 export default {
-	input: ['./wwwroot/js/**/*.js'],
-	output: {
-		format: 'esm',
+  input: ['./wwwroot/js/**/*.js'],
+  output: {
+    format: 'esm',
     dir: '.',
-	},
+  },
   plugins: [
-		multiInput.default(),
-		terser(),
-		commonjs(),
-		nodeResolve() 
-	],
+    multiInput.default(),
+    terser(),
+    commonjs(),
+    nodeResolve() 
+  ],
 };
