354515d96a2dd9d6c70ee18f4722f7da3341bb5b
Huy Ngo <huybngo@yahoo.com>
2024-04-09 10:02:48 -0700

    Update rollup config to handle node_modules in wwwrooot

--- src/rollup.config.js
+++ src/rollup.config.js
@@ -3,16 +3,20 @@ import terser from '@rollup/plugin-terser';
 import nodeResolve from '@rollup/plugin-node-resolve';
 import commonjs from "@rollup/plugin-commonjs";
 
+const DistDir = './wwwroot/js';
+
 export default {
-  input: ['./wwwroot/js/**/*.js'],
+  // Input is the output of tsc i.e the
+  // unbundled & unminified JS files
+  input: [`${DistDir}/**/*.js`],
   output: {
     format: 'esm',
-    dir: '.',
+    dir: DistDir,
   },
   plugins: [
-    multiInput.default(),
+    multiInput.default({ relative: DistDir }),
     terser(),
     commonjs(),
-    nodeResolve() 
+    nodeResolve()
   ],
 };
