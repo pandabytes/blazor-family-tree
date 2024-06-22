ad0e49e22c8da0f4b0c8875e3ad127dbd93a1c5c
Huy Ngo <huybngo@yahoo.com>
2024-06-22 11:53:19 -0700

    Add tests for family tree static module

--- tests/Blazor.FamilyTreeJS.Tests/Usings.cs
+++ tests/Blazor.FamilyTreeJS.Tests/Usings.cs
@@ -1,5 +1,8 @@
+global using Microsoft.Extensions.DependencyInjection;
+
 global using Bunit;
 global using Xunit;
 
 global using Blazor.FamilyTreeJS.Components;
 global using Blazor.FamilyTreeJS.Components.Interop.Converters;
+global using Blazor.FamilyTreeJS.Components.Interop.Modules.FamilyTreeStatic;
